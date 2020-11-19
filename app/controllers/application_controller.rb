15224389ac87621b228eb6d87c07d37d7a7fea25
Dominic Barajas <dominicbarajas548@gmail.com>
2020-11-19 18:04:43 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -24,8 +24,23 @@ class ApplicationController < Sinatra::Base
     erb :edit
   end
 
-  #delete '/recipes/:id' do
-    #@recipe = Recipe.find_by_id(params[:id])
-    #@recipe.delete
-  #end
+  patch '/recipes/:id' do
+    @recipe = Recipe.find_by_id(params[:id])
+    @recipe.name = params[:name]
+    @recipe.ingredients = params[:ingredients]
+    @recipe.cook_time = params[:cook_time]
+    @recipe.save
+    redirect to "/recipes/#{@recipe.id}"
+  end
+
+  post '/recipes' do
+    @recipe = Recipe.create(params)
+    redirect to "/recipes/#{@recipe.id}"
+  end
+
+  delete '/recipes/:id' do
+    @recipe = Recipe.find_by_id(params[:id])
+    @recipe.delete
+    redirect to '/recipes'
+  end
 end
